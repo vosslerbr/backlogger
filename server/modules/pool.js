32741e3d2cc273b2c7a5fb59f977479b20727bc5
Ryan Bennek <rbennek.rb@gmail.com>
2025-10-25 17:54:05 -0500

    windows env setup

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,5 +1,20 @@
 // Connects db to project
-import pg from "pg";
+import pg from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
+
+//env
+console.log('Environment variables loaded:', {
+    SQL_USER: process.env.SQL_USER,
+    SQL_PASS: process.env.SQL_PASS,
+    ENV_FILE_PATH: process.env.NODE_ENV,
+    CWD: process.cwd()
+});
+
+const user = process.env.SQL_USER;
+const pass = process.env.SQL_PASS;
+console.log(pass);
+
 let pool;
 
 if (process.env.DATABASE_URL) {
@@ -13,11 +28,14 @@ if (process.env.DATABASE_URL) {
 // When we're running this app on our own computer
 // we'll connect to the postgres database that is
 // also running on our computer (localhost)
+//posgres port 5432
 else {
   pool = new pg.Pool({
-    host: "localhost",
-    port: 5433,
-    database: "backlogger",
+    user: user,
+    password: pass,
+    host: 'localhost',
+    port: 5432,
+    database: 'backlogger',
   });
 }
 
--- server/server.js
+++ server/server.js
@@ -1,25 +1,26 @@
 //. && nodemon --exec tsx ./server/server.ts
 
-import express from "express";
-import dotenv from "dotenv";
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 const port = 3000;
 
 // Routes
-import gamesRouter from "./routes/games.router.js";
+import gamesRouter from './routes/games.router.js';
 
 // Express Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("build"));
+app.use(express.static('build'));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
 // Routes
-app.use("/api/games", gamesRouter);
+app.use('/api/games', gamesRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
